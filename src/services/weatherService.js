28363830dfba2c0ba04a5956d8330c9aec7deb0f
NaufalFiqri <naufal11bale@gmail.com>
2023-05-25 22:52:29 +0800

    connected weather api

---
+++ src/services/weatherService.js
@@ -0,0 +1,15 @@
+const API_KEY = "7d972b28fc95af77fe1871b15c651a67";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+const getWeatherData = (infoType, searchParams) => {
+  const url = new URL(BASE_URL + "/" + infoType);
+  url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData;
