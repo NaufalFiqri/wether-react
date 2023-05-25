3f688c2fb045d3e7142a54862c2363ef58794e91
NaufalFiqri <naufal11bale@gmail.com>
2023-05-25 22:24:53 +0800

    init forecast section

---
+++ src/components/Forecast.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Forecast() {
+  return (
+    <div>
+      <div className='flex item-center justify-center-start mt-6'>
+        <p className='text-white font-medium uppercase'>hourly foreceast</p>
+      </div>
+      <hr className='my-2'/>
+    </div>
+  )
+}
+
+export default Forecast
---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Monday, 15 May 2023 | Local time: 4:04pm
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Berlin, DE</p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
