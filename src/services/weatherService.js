e0ae3b727801dc715330ed161ab6c4caaf739d41
NaufalFiqri <naufal11bale@gmail.com>
2023-05-25 22:58:50 +0800

    abc

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -7,9 +7,9 @@ const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
 
-  return fetch(url)
-    .then((res) => res.json())
-    .then((data) => data);
+  return fetch(url).then((res) => res.json());
 };
 
-export default getWeatherData;
+const getFormattedWeatherData = (searchParams) => {
+    const fotmattedCurrentWeather = await getWeatherData('weather')
+}
